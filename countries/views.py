d48986b60dc1f855c799d0ca133236c2781ecc12
Jhonbeltran <jbeltranleon@gmail.com>
2018-07-15 16:15:49 -0500

    agrego implementacion 1 de Template view

--- countries/views.py
+++ countries/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View
+"""from django.views.generic import View"""
+##Comportamiento por defecto
+from django.views.generic import TemplateView
 
 #Vistas basadas en Funciones (Function Based View)
 """def home(request):
 	return render(request, 'countries/home.html')"""
 
 #Vistas basadas en Clases (Class Based View)
-class HomeView(View):
+#class HomeView(View):
+##Comportamiento por defecto
+class HomeView(TemplateView):
 	#Dependiendo el tipo de respuesta que necesitemos implementamos la funcion
 	#get o post
-	def get(self, request, *args, **kwargs):
-		return render(request, 'countries/home.html')
+	#def get(self, request, *args, **kwargs):
+	#	return render(request, 'countries/home.html')
+	##Comportamiento por defecto
+	template_name = 'countries/home.html'
